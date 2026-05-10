d8604bd071ffe4dabda03d2de6947fbcf135e6f9
Tejasshelke1770 <shelket17@gmail.com>
2026-05-11 00:44:32 +0530

    feature post creation completed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,11 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import authRouter from "./routes/auth.routes.js";
+import postRouter from "./routes/post.routes.js";
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const app = express();
 
@@ -11,6 +16,7 @@ app.get("/", (req, res) => {
   res.send("Hello Instagram");
 });
 
-app.use('/api/auth', authRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/post", upload.single("image"), postRouter);
 
 export default app;
