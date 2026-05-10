7e3cf48be5eef9c2df4759a17aa4f53d5a9b8172
Tejasshelke1770 <shelket17@gmail.com>
2026-05-11 03:00:52 +0530

    post Routes completed

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import postModel from "../models/post.model.js";
 import { uploadImage } from "../services/imageKit.js";
 
@@ -20,6 +21,49 @@ export const createPost = async (req, res) => {
   });
   return res.status(201).json({
     message: "Post created!",
+    post,
+  });
+};
+
+export const getPosts = async (req, res) => {
+  const userId = req.user._id;
+
+  const posts = await postModel.find({ userId: userId });
+
+  return res.status(200).json({
+    message: "post fetched successfully",
+    posts,
+  });
+};
+
+export const getPostDetails = async (req, res) => {
+  const userId = req.user._id;
+  const postId = req.params.postId;
+
+  if (!mongoose.Types.ObjectId.isValid(postId)) {
+    return res.status(400).json({
+      message: "Invalid Post Id",
+    });
+  }
+
+  const post = await postModel.findById(postId);
+
+  if (!post) {
+    return res.status(404).json({
+      message: "post not found!",
+    });
+  }
+
+  const isValidUser = post.userId.toString() == userId.toString();
+
+  if (!isValidUser) {
+    return res.status(403).json({
+      message: "Forbidden Content",
+    });
+  }
+
+  return res.status(200).json({
+    message: "post fetched Successfully",
     post,
   });
 };
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,9 +1,20 @@
 import express from "express";
-import { createPost } from "../controllers/post.controller.js";
+import multer from "multer";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import {
+  createPost,
+  getPostDetails,
+  getPosts,
+} from "../controllers/post.controller.js";
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 const postRouter = express.Router();
 
-postRouter.post("/", authMiddleware, createPost);
+postRouter.post("/", upload.single("image"), authMiddleware, createPost);
+
+postRouter.get("/", authMiddleware, getPosts);
+
+postRouter.get("/detail/:postId", authMiddleware, getPostDetails);
 
 export default postRouter;
