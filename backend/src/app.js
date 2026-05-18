e7499a1452760f14b39e5ceed79a6f82611d37c6
Tejasshelke1770 <shelket17@gmail.com>
2026-05-19 03:17:47 +0530

    cors added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRouter from "./routes/auth.routes.js";
 import postRouter from "./routes/post.routes.js";
@@ -9,6 +10,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello Instagram");
